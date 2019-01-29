5eb58048601738b86b2f818d1f6b75b69a7fb1a9
Kirkland <kirkland@yuknis.com>
2019-01-29 09:44:22 -0500

    Renamed default application to JWebTrap

--- src/main/java/com/yuknis/jwebtrap/JWebTrap.java
+++ src/main/java/com/yuknis/jwebtrap/JWebTrap.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JWebTrap {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JWebTrap.class, args);
 	}
 
 }
